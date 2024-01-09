b5b28074ff96465720175812815b07743eadd305
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-09 22:58:52 +0600

    react icons have been installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import quotes from "./assets/quotes.json";
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import './App.css';
 
 
@@ -17,7 +18,21 @@ function App() {
   const [quote, setQuote] = useState<Quote>(getRandomQuote());
  
 
-  return <>{quote.quote}</>
+  return (
+    <div>
+      <div id='quote-box'>
+        <div className='quote-content'>
+          <FaQuoteLeft size="30" style={{ marginRight: "10px"}}/>
+          <h2 id='text'>{quote.quote}</h2>
+          <FaQuoteRight size="30" style={{ marginLeft: "10px"}}/>
+         
+          <h4 id='author'>- {quote.author}</h4>
+
+        </div>
+
+      </div>
+    </div>
+  )
   
 }
 
