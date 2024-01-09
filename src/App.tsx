90be113a0b2c0b6495ad96cfd08c4bfcd8157380
Zahid Hasan <zahidhasan140338@gmail.com>
2024-01-09 22:37:33 +0600

    quotes have been integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+
+interface Quote {
+  quote: string;
+  author: string;
+}
+
+const getRandomQuote = (): Quote => {
+  return quotes 
+}
 
 function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
  
 
-  return (
-    <>
-     hello
-    </>
-  )
+  return <>hello</>
+  
 }
 
 export default App
